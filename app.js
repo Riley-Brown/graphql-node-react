09897cc757255554b4d4bc8654904e6600abeca9
Riley-Brown <riley@riley.gg>
2019-06-05 23:49:26 -0400

    imported mongoose model and set event query to save to DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
 
@@ -43,14 +44,28 @@ app.use(
         return events;
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.description,
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventInput.description,
+        //   price: args.eventInput.price,
+        //   date: args.eventInput.date
+        // };
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
           price: args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save(event)
+          .then(res => {
+            console.log(res);
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
